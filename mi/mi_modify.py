6be616ed4c1901240710fd4c76d07f886111a444
AugustTheodor.Cat <49180658+AugustineFulgur@users.noreply.github.com>
2025-09-29 15:40:21 +0800

    0.0.7.1
    FIX

--- mi/mi_modify.py
+++ mi/mi_modify.py
@@ -18,18 +18,17 @@ class Ctx_head(Ctx_base):
         self.do_curd(flow.response)
 
     def do_curd(self,req):
-        for i in self.head:
-            if self.curd==CURD.ADD:
-                req.headers[i.key]=i.value
-            elif self.curd==CURD.DELETE:
-                del req.headers[i.key]
-            elif self.curd==CURD.REPLACE:
-                # 从一些角度上必要但另一些角度上不必要的东西
-                req.headers[i.key]=i.value
-            elif self.curd==CURD.LOOKUP:
-                # CAUTION
-                # 请在这里修改以适配
-                print(req.headers[i.key])
+        if self.curd==CURD.ADD:
+            req.headers[self.s1]=self.s2
+        elif self.curd==CURD.DELETE:
+            del req.headers[self.s1]
+        elif self.curd==CURD.REPLACE:
+            # 从一些角度上必要但另一些角度上不必要的东西
+            req.headers[self.s1]=self.s2
+        elif self.curd==CURD.LOOKUP:
+            # CAUTION
+            # 请在这里修改以适配
+            print(req.headers[self.s1])
 
 class Ctx_content(Ctx_base):
 
@@ -48,12 +47,11 @@ class Ctx_content(Ctx_base):
         self.do_curd(flow.response)
 
     def do_curd(self,req):
-        for i in self.head:
-            if self.curd==CURD.DELETE:
-                req.content.replace(self.s,"")
-            elif self.curd==CURD.REPLACE:
-                # 从一些角度上必要但另一些角度上不必要的东西
-                req.content.replace(self.s,self.s2)
+        if self.curd==CURD.DELETE:
+            req.text.replace(self.s,"")
+        elif self.curd==CURD.REPLACE:
+            # 从一些角度上必要但另一些角度上不必要的东西
+            req.text.replace(self.s,self.s2)
 
 class Ctx_all(Ctx_base):
     
--- run.py
+++ run.py
@@ -1,13 +1,13 @@
 from etc.base import *
 from mi.mi_crypt import *
-from mi.mi_head import *
+from mi.mi_modify import *
 from mi.mi_code import *
 from mi.mi_notrace import *
+from mi.mi_webpack import *
 from enum import Enum
 
+token=""
 
 addons = [
-    #Ctx_encrypt("a=([0-9]*)",[RR.REQUEST],ALGO.AES,AES.MODE_CBC,'1234567887654321'.encode('utf-8'),CODE.BASE64,'utf-8','1234567887654321'.encode('utf-8')),
-    Ctx_ua(UA.WXMINIPROGRAM),
-    Ctx_drop_wechat301(),
+    Ctx_forcejs()
 ]
