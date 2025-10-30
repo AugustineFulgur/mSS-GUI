b1d1906fb11be86776a10e806d6a26c0598895e3
AugustTheodor.Cat <49180658+AugustineFulgur@users.noreply.github.com>
2025-10-30 09:06:55 +0800

    0.1.3.2
    【废弃】 esprima + escodegen
    
    但是没用 我已经悟到了 如果不想面对连绵不断的粑粑就直接用node

--- etc/jsast.py
+++ etc/jsast.py
@@ -1,32 +1,47 @@
-# 使用AST功能需要额外导入esprima 我会补充到requirements里
-from visitor import *
-from abc import ABC,abstractmethod
+from __future__ import unicode_literals
+from esprima.visitor import Visitor, Visited
+from esprima.nodes import *
 import esprima
 import escodegen
-from mi.mi_gui import Ctx_gui
 
+# 这Visitor的逻辑不是人看的 ^ ^
+class AST(Visitor):
 
-
-class AST(ABC): # 生活不如意，就写pythonOOP
-
-    # 匹配函数
-    @abstractmethod
-    def expr(self,node:Node) -> bool:
-        pass
-
-    # 执行函数 要扩展输入的时候重载或者多写个成员变量就行
-    @abstractmethod
-    def do(self,node:Node) -> None:
-        pass
-
-    def bake(self,js:str,url:str="") -> None:
+    def __init__(self,js:str):
+        super().__init__()
+        self.js=js
+        self.in_target_func = False
+        self.code=None
         try:
-            program = objectify(esprima.parseScript(js).toDict())
-            for node in program.traverse():
-                if self.expr(node): self.do(node)
-            print(escodegen.generate(program))
-
+            self.code=esprima.parseScript(self.js)
         except:
             pass
-            Ctx_gui.logger(f"AST发生错误，位置:{url or '未知'}")
+        finally:
+            self.code=esprima.parseModule(self.js)
+        self.jsafter=self.visit(self.code)
 
+    def visit_FunctionExpression(self, node):
+        prev_state = self.in_target_func
+        self.in_target_func = (node.id is None) and (len(node.params) == 3)
+        result = yield node.__dict__  
+        self.in_target_func = prev_state
+        yield Visited(result)  
+
+    def visit_CallExpression(self, node):
+        if self.in_target_func:
+            if (isinstance(node.callee, Identifier) and node.callee.name == "eval" and isinstance(node.arguments[0],Literal)):
+                # 改这里
+                yield Visited(esprima.parseScript(node.arguments[0].value).body[0].expression)
+                return # 写得我快飞升了，主要你py的yield跟其它语言的yield不是一个yield
+        result = yield node.__dict__
+        yield Visited(result)
+
+if __name__ == "__main__":
+    
+
+    with open("1.js","r",encoding="utf8") as f:
+        js_code=f.read()
+
+    extractor = AST(js_code)
+    with open("2.js","w",encoding="utf8") as f:
+        f.write(escodegen.generate(extractor.jsafter.toDict()))
